9a1b0eaf96bb4e06521014e096bdfb2cff78f71e
raveenkarri <131150835+raveenkarri@users.noreply.github.com>
2025-11-24 00:45:58 +0530

    Add CORS configuration for specific origins
    Configured CORS to allow specific origins.

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,14 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://inventory-management-app-nine-kappa.vercel.app/'],
+    credentials: false
+  })
+);
+
+
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 app.get('/api/products', async (req, res) => {
